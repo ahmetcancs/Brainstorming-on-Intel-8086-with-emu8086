9682887e01a64e9afee3c0552a34a840743ac1b7
Ahmet CAN <57818166+ahmetcancs@users.noreply.github.com>
2022-11-21 00:56:13 +0300

    Update DataAdressingWithBasePlusIndex_3.asm

--- DataAdressingWithBasePlusIndex_3.asm
+++ DataAdressingWithBasePlusIndex_3.asm
@@ -10,23 +10,24 @@ main proc
         MOV AX,0050H
         MOV DI,AX
         MOV AX,0000H
-		MOV	[0050H],04H	
-		MOV	[0051H],00H	
-		MOV	[0052H],0FCH
-		MOV	[0053H],00H	
-		MOV	[0054H],05H
-		MOV	[0150H],00H	
-		MOV	[0151H],02H	
+	
+	MOV	[0050H],04H	
+	MOV	[0051H],00H	
+	MOV	[0052H],0FCH
+	MOV	[0053H],00H	
+	MOV	[0054H],05H
+	MOV	[0150H],00H	
+	MOV	[0151H],02H	
         MOV	[0152H],04H
         MOV	[0153H],06H
         MOV	[0154H],08H
         
 
-		MOV	DX,[0050H]	 
-		MOV	CX,[0052H]	
-		ADD DX,CX
-		MOV BX,DX
-		MOV AL,00
+	MOV	DX,[0050H]	 
+	MOV	CX,[0052H]	
+	ADD DX,CX
+	MOV BX,DX
+	MOV AL,00
 			    
 ADD_LP:	ADD	AL,[BX+DI]
 		INC	BX	        
